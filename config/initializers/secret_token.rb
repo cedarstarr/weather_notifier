5d4996b9ab302fcba8384cdddca5f6494552614b
Prelang Builder <builder@prelang.com>
2014-05-21 16:48:20 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeatherNotifier::Application.config.secret_key_base = '565ea3b0e3e4cb7a24d1ce868041fdf18635a83e6b596b9d6a905bd58ecc5a169a9e0cd9a593451024a3e7cbe7d8c604250f4a52c66b0841dbbd31e2060b5b49'
